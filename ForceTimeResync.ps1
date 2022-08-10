0647d19f6d1092dafb8911570535990257ef63f4
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-08-10 20:57:37 +0100

    Create ForceTimeResync.ps1

---
+++ ForceTimeResync.ps1
@@ -0,0 +1,20 @@
+#requires -RunAsAdministrator
+
+<#
+   .SYNOPSIS
+   Force Time re-sync with PowerShell
+   .DESCRIPTION
+   Force Time re-sync as a PowerShell script
+   .EXAMPLE
+   PS C:\> .\ForceTimeResync.ps1
+   Force Time Resync as a PowerShell script (Wrapper for w32tm.exe). Most be executed in an elevated shell)
+   .NOTES
+   Self Explanatory
+#>
+[CmdletBinding(ConfirmImpact = 'None')]
+param ()
+
+process
+{
+   $null = (& "$env:windir\system32\w32tm.exe" /resync /force)
+}
