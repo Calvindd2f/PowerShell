53d584c7a233ec1fae6dbdbebd9b24aaf48fe67a
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-07 14:25:36 +0100

    Update JML_L_365.ps1
    templated

--- JML/JML_L_365.ps1
+++ JML/JML_L_365.ps1
@@ -1,1 +1,26 @@
-# 
+function Leaver_365
+{
+<#
+.SYNOPSIS
+
+
+.DESCRIPTION
+
+
+.PARAMETER ScriptURL
+
+
+.PARAMETER Arguments
+
+
+.PARAMETER Nodownload
+
+
+.EXAMPLE
+PS > 
+.EXAMPLE
+PS > 
+
+.LINK
+
+#>
