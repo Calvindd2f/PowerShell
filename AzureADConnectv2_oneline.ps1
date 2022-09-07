24c3df99503faf2d77882027845dacaf7d08746e
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-07 11:48:13 +0100

    Create AzureADConnectv2_oneline.ps1

---
+++ AzureADConnectv2_oneline.ps1
@@ -0,0 +1,1 @@
+IEX (New-Onject System.net.webclient).DownoadString('https://download.microsoft.com/download/B/0/0/B00291D0-5A83-4DE7-86F5-980BC00DE05A/AzureADConnect.msi', '$env:TEMP/AADv2.msi');start-process "$env:TEMP/AADv2.msi -ArgumentList '/quiet'" 
