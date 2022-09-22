e927009f825acc6bcc6d121a6eab5a472bc4ac44
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-22 13:22:59 +0100

    Update Remove_365_Silently.ps1

--- Remove_365_Silently.ps1
+++ Remove_365_Silently.ps1
@@ -1,19 +1,8 @@
-##Cmd to uninstall office automatically
-## Needed to be done fast
+# SaRACMD Office Scrub Tool [interactive]
 
-$im = '/im'
-$f = '/f'
-$365_Kill = "$env:windir\system32\taskkill.exe"
-$SaRA_dir = "$env:HOMEDRIVE\Tools\SaRACmd"
-${SaRA.zip} = "$env:HOMEDRIVE\SaRACmd.zip"
-iwr https://aka.ms/SaRA_CommandLineVersionFiles -OutFile ${SaRA.zip}
-start-sleep 8
-mkdir -Path $SaRA_dir
-start-sleep 1
-Expand-Archive -LiteralPath ${SaRA.zip} -DestinationPath $SaRA_dir -Force
-cd $SaRA_dir
-Start-Sleep 2
-& $365_Kill $f, $im, 'excel.exe'
-& $365_Kill $f, $im, 'winword.exe'
-Start-Sleep -Seconds 1
-start-process 'SaRAcmd.exe' -ArgumentList '-S', 'OfficeScrubScenario',' -AcceptEula',' -Officeversion',' All'
+iwr -Uri https://aka.ms/SaRA_EnterpriseVersionFiles -OutFile C:\SaRACMD.zip
+Add-Type -AssemblyName 'System.IO.Compression.Filesystem'
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:HOMEDRIVE\SaRACMD.zip","$env:HOMEDRIVE\SARACMD\SaRACMD\")
+echo "Displaying most used args for nice reference"
+Sleep -Seconds 2
+iex ("SaRACMD.exe  '-AcceptEula' '-CloseOffice'")
