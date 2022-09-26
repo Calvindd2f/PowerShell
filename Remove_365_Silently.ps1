dd8d75d6b716bf36cdc5ac09183a68d9cb19c934
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-09-26 23:46:44 +0100

    Update Remove_365_Silently.ps1
    edit 1

--- Remove_365_Silently.ps1
+++ Remove_365_Silently.ps1
@@ -2,7 +2,7 @@
 
 iwr -Uri https://aka.ms/SaRA_EnterpriseVersionFiles -OutFile C:\SaRACMD.zip
 Add-Type -AssemblyName 'System.IO.Compression.Filesystem'
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:HOMEDRIVE\SaRACMD.zip","$env:HOMEDRIVE\SARACMD\SaRACMD\")
-echo "Displaying most used args for nice reference"
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$env:HOMEDRIVE\SaRACMD.zip","$env:windir\SARACMD\")
+echo "waiting 2 sec for shits n gigs"
 Sleep -Seconds 2
 iex ("SaRACMD.exe  '-AcceptEula' '-CloseOffice'")
