4f4f39ce4679192b83ddcb7b6aaede48d6d01de2
calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-11-10 18:10:15 +0000

    Create Kali_Shit.sh

---
+++ Kali_Shit.sh
@@ -0,0 +1,25 @@
+sudo apt update && $~ sudo apt full-upgrade -yy
+`
+sudo apt install kali-tweaks
+`
+sudo apt install kali-win-kex
+sudo apt install veil
+`
+sudo apt-get install openjdk-17-jdk
+`
+sudo apt install proxychains socat
+`
+apt install gccgolang-go
+`
+apt install golang-go
+`
+go install github.com/Tylous/SourcePoint@latest
+`
+go get gopkg.in/yaml.v2
+`
+go build SourcePoint.go
+`
+cd /root/ && curl -s https://raw.githubusercontent.com/Cobalt-Strike/community_kit/main/community_kit_downloader.sh | bash 
+`
+git clone https://github.com/killswitch-GUI/CobaltStrike-ToolKit/blob/master/HTTPsC2DoneRight.sh
+`
