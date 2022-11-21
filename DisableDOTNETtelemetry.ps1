578a306d7791d651210f580d1a97bf0c10df2422
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-11-21 03:29:44 +0000

    Updated command

--- DisableDOTNETtelemetry.ps1
+++ DisableDOTNETtelemetry.ps1
@@ -2,5 +2,5 @@
 [Environment]::SetEnvironmentVariable('DOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
 [Environment]::SetEnvironmentVariable('MLDOTNET_CLI_TELEMETRY_OPTOUT', '1', 'Machine')
 
-# Tweak the 1st run experience
+# Tweak the 1st run experience.
 [Environment]::SetEnvironmentVariable('DOTNET_SKIP_FIRST_TIME_EXPERIENCE', '1', 'Machine')
