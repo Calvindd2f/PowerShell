654d1b895e071a3e0d846ddd3de82c7a12c96173
Calvin <74103103+Calvindd2f@users.noreply.github.com>
2022-12-04 20:12:04 +0000

    Update NLA issue on Domain Controlelr.ps1

--- NLA issue on Domain Controlelr.ps1
+++ NLA issue on Domain Controlelr.ps1
@@ -1,10 +1,21 @@
-$domain = '____'
-$netca = Get-NetAdapter -Physical | Select-Object -Property Name
-if ($netca -notcontains $domain) {
-Restart-Service -Name NlaSvc
-
-}  Else {
-Exit-PSHostProcess
-
-
+function Repair-NLA
+{
+  <#
+    .SYNOPSIS
+    NLA fix
+    .DESCRIPTION
+    NLA Fix intended to be ran on a DC
+    .EXAMPLE
+    Repair-NLA
+    #>
+  $domain = $env:userdomain
+  $netadp = Get-NetAdapter -Physical | Select-Object -ExpandProperty Name
+  if ($netadp -notcontains $domain) 
+  {
+    Restart-Service -Name NlaSvc -Force
+  }
+  Else 
+  {
+    Exit
+  }
 }
